a2fdfa1860dffd5e72427ee301d393d2b8e1510b
mu <masato.umeno.818@gmail.com>
2026-01-05 19:06:55 +0900

    feat: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
+import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://01-mu.dev',
+  integrations: [mdx()],
 });
